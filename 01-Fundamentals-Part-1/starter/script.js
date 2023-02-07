8505fb062bbb1b6ee1ac712caddb5e95c6cea980
hnguyendh <hnguyendh1802@gmail.com>
2023-02-07 20:21:47 +0700

    20230207

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = 78/(1.69**2);
+const johnBMI = 92/(1.95*1.95);
+
+console.log(markBMI, johnBMI);
+let result;
+if (markBMI > johnBMI) {
+    result = "Mark's BMI is higher than John's"
+} else {
+    result = "John's BMI is higher than Mark's"
+}
+console.log(result);
+result = `Mark's BMI ${markBMI} is higher than John's${johnBMI}!`
+console.log(result);
