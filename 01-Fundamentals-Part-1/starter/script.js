6aae7f83811aa8fa4fa7cabd203a2edc8e3d4c28
hnguyendh <hnguyendh1802@gmail.com>
2023-02-14 22:58:27 +0700

    20230214

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,29 +1,44 @@
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
 
-const markBMI = 78/(1.69**2);
-const johnBMI = 92/(1.95*1.95);
+// const markBMI = 78/(1.69**2);
+// const johnBMI = 92/(1.95*1.95);
 
-console.log(markBMI, johnBMI);
-let result;
-if (markBMI > johnBMI) {
-    result = "Mark's BMI is higher than John's"
+// console.log(markBMI, johnBMI);
+// let result;
+// if (markBMI > johnBMI) {
+//     result = "Mark's BMI is higher than John's"
+// } else {
+//     result = "John's BMI is higher than Mark's"
+// }
+// console.log(result);
+// result = `Mark's BMI ${markBMI} is higher than John's${johnBMI}!`
+// console.log(result);
+
+
+// const favorite = Number(prompt("What 's your favorite number"));
+// console.log(favorite);
+// console.log(typeof favorite );
+
+// if (favorite == 23){
+//     console.log("Cool! 23 is an amazing number")
+// } else {
+//     console.log("Nothings!");
+// }
+
+const hasDriverLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+if (shouldDrive) {
+    console.log('Sarah is able to drive!');
 } else {
-    result = "John's BMI is higher than Mark's"
-}
-console.log(result);
-result = `Mark's BMI ${markBMI} is higher than John's${johnBMI}!`
-console.log(result);
-
-
-const favorite = Number(prompt("What 's your favorite number"));
-console.log(favorite);
-console.log(typeof favorite );
-
-if (favorite == 23){
-    console.log("Cool! 23 is an amazing number")
-} else {
-    console.log("Nothings!");
+    console.log('Someone else should drive...');
 }
