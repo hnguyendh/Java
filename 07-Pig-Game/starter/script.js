b7205c71b7b5d007b67becb2701a2e067cb1f95f
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2023-03-21 19:29:24 +0700

    Mac 20230320

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// Select elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice function
+btnNew.addEventListener('click', function () {});
