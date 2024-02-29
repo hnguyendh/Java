fa701e5fd162f059ad313767f13403a473bc221e
hnguyendh <hnguyendh1802@gmail.com>
2024-02-29 09:29:30 +0700

    lar_pc - can remove

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,20 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const allSections = document.querySelectorAll('section');
+console.log(allSections);
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+// Creating and inserting elements
+// .insertAdjacentHTML
+const message = document.createElement('div');
+const header = document.querySelector('.header');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookie for improved functionality and analytics';
+message.innerHTML =
+  'We use cookie for improved functionality and analytics. <button class ="btn btn--close-cookie"> Got it! </button>';
+header.prepend(message);
